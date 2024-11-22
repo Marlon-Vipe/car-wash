a4b73b1461c675a9c31d2f886d3824f036a54417
Marlon Villalona <iamvillalona@gmail.com>
2024-11-22 18:03:21 -0400

    added modules and cars component

---
+++ src/app/modules/models/cars.ts
@@ -0,0 +1,12 @@
+export interface Cars {
+    id: number;
+    title: string;
+    last_bid?: number;
+    price: number;
+    creator?: string;
+    avatar?: string;
+    instant_price?: number;
+    ending_in?: string;
+    image: string;
+  }
+  
